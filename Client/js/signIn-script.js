0aefe5597205c6940f7b64c16de4924c781348c1
Manaila Robert <robert122003@gmail.com>
2024-01-13 00:35:05 +0200

    working on UI v0.6

--- Client/js/signIn-script.js
+++ Client/js/signIn-script.js
@@ -1,18 +1,12 @@
 function validateForm(){
     const email = document.getElementById("email").value
-    console.log(email)
     const password = document.getElementById("password").value
-    const strengthBarValue =document.getElementById("strength"). value
     
     document.getElementById("email-error").innerHTML = ""
     document.getElementById("password-error").innerHTML = ""
     
     let isValid = true;
     
-    if(username === ""){
-        document.getElementById("name-error").innerHTML = "Username Invalid"
-        isValid =false
-    }
 
 //email validation
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
@@ -25,14 +19,13 @@ function validateForm(){
     {
         document.getElementById("email-error").innerHTML = "Email Invalid"
         isValid =false
-        console.log(email.match(pattern))
     }
      
 
 //password validation
     if(password === ""){
         document.getElementById("password-error").innerHTML = "Password Invalid"
-    isValid =false
+        isValid =false
     }
    return isValid
 }
